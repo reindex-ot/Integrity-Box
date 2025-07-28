4acb32c71bd42c4efdfe7f8b644f5727e28402e5
MONA / <121730102+MeowDump@users.noreply.github.com>
2025-07-27 19:29:09 -0700

    ‎

---
+++ webroot/lang/en.js
@@ -0,0 +1,69 @@
+// Language: English
+// Translated by: Mona @TempMeow
+
+// [SECTION 1] Translate the text inside " "
+export const translations = {
+  "en": [
+    "Disable Whitelist Mode",
+    "Enable Whitelist Mode",
+    "Set Custom PIF fingerprint",
+    "Spoof/Unspoof PIF Injection Value",
+    "Spoof/Unspoof PIF Fork Value",
+    "Kill GMS Process",
+    "Fix Device Not Certified",
+    "Update Keybox",
+    "Add All Apps to Target List",
+    "Add Only User Apps to Target List",
+    "Switch to Default Keybox",
+    "Switch to Open Source Keybox",
+    "Spoof Tricky Store Patch",
+    "Fetch Banned Keybox List",
+    "Update SusFS Configuration",
+    "Set Verified Boot Hash",
+    "Enable Inbuilt GMS Spoofing",
+    "Hide Spoofing Prop Detection",
+    "PixelOS Spoofing",
+    "Xiaomi EU Spoofing",
+    "HentaiOS Spoofing",
+    "Derpfest Spoofing",
+    "Abnormal Detection",
+    "Flagged Apps",
+    "Props Detection",
+    "Support the Developer",
+    "Join Update Channel",
+    "Join Help Group",
+    "Source Code",
+    "Report Issue/Bug",
+    "Toggle SELinux Status",
+    "Module Info",
+    "Resize WebUI",
+    "Modal Auto Close Switch",
+    "Play Game"
+  ]
+};
+
+// [SECTION 2] Translate the text inside { }
+export const buttonGroups = {
+  "Whitelist": { "en": "Whitelist" },
+  "PIF": { "en": "Play Integrity Fix" },
+  "TS": { "en": "Tricky Store" },
+  "Sus": { "en": "SusFS" },
+  "CrSpoof": { "en": "Custom ROM Spoofer" },
+  "Check": { "en": "Diagnostic" },
+  "Help": { "en": "Community & Support" },
+  "Basic": { "en": "Basic" },
+  "TimePass": { "en": "Game" }
+};
+
+// [SECTION 3] There's nothing to translate in this section
+export const buttonOrder = [
+  "stop.sh", "start.sh", 
+  "pif.sh", "spoof.sh", "piffork.sh", "kill.sh", "vending.sh",  "key.sh",
+  "systemuser.sh", "user.sh", "keybox.sh", "aosp.sh", "patch.sh", "banned.sh",
+  "sus.sh", "boot_hash.sh",
+  "setprop.sh", "resetprop.sh", "pixelos.sh", "xiaomi.sh", "helluva.sh", "derpfest.sh",
+  "abnormal.sh", "app.sh", "prop.sh",
+  "support", "meowdump.sh", "meowverse.sh", "info.sh", "report.sh",
+  "selinux.sh", "module_info.sh", "resize.sh", "modal.sh",
+  "game.sh"
+];
