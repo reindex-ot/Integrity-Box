644bcf5149123024116e1f0f55697a9cbd6e0111
MONA / <121730102+MeowDump@users.noreply.github.com>
2025-07-30 21:51:19 -0700

    ‎

---
+++ webroot/lang/sr.js
@@ -0,0 +1,69 @@
+// Language: Српски
+// Translated by: Robilad
+
+// [SECTION 1] Преведи текст унутар " "
+export const translations = {
+  "sr": [
+    "Онемогући режим аутоматског белог списка",
+    "Омогући режим аутоматског белог списка",
+    "Постави прилагођени PIF prop/json",
+    "Лажирај/Врати оригиналне вредности PIF Injection",
+    "Лажирај/Врати оригиналне вредности PIF Fork",
+    "Угаси GMS процес",
+    "ПОПРАВИ Уређај није сертификован",
+    "Ажурирај Keybox",
+    "Додај све апликације у циљану листу",
+    "Додај само корисничке апликације у циљану листу",
+    "Пребаци на подразумевани Keybox",
+    "Пребаци на AOSP Keybox",
+    "Лажирај Tricky Store закрпу",
+    "Преузми листу забрањених Keybox-а",
+    "Ажурирај SusFS конфигурацију",
+    "Постави хеш провереног покретања",
+    "Омогући уграђено GMS лажирање",
+    "Сакриј GMS откривање променљивих",
+    "PixelOS лажирање",
+    "Xiaomi EU лажирање",
+    "HentaiOS лажирање",
+    "Derpfest лажирање",
+    "Аномално откривање",
+    "Означене апликације",
+    "Откривање променљивих",
+    "Подржи програмера",
+    "Придружи се каналу за ажурирања",
+    "Придружи се групи за помоћ",
+    "Изворни код",
+    "Пријави проблем/баг",
+    "Промени SELinux статус",
+    "Информације о модулу",
+    "Промени величину WebUI",
+    "Ауто-затварање модалног прозора",
+    "Играј игру"
+  ]
+};
+
+// [SECTION 2] Преведи текст унутар { " " }
+export const buttonGroups = {
+  "Whitelist": { "sr": "Shamiko и NoHello" },
+  "PIF": { "sr": "Play Integrity Fix" },
+  "TS": { "sr": "Tricky Store" },
+  "Sus": { "sr": "SusFS" },
+  "CrSpoof": { "sr": "Лажирање ROM-ова" },
+  "Check": { "sr": "Дијагностика" },
+  "Help": { "sr": "Заједница и Подршка" },
+  "Basic": { "sr": "Опште" },
+  "TimePass": { "sr": "Игра" }
+};
+
+// [SECTION 3] У овом одељку нема ничега што треба превести
+export const buttonOrder = [
+  "stop.sh", "start.sh", 
+  "pif.sh", "spoof.sh", "piffork.sh", "kill.sh", "vending.sh",  "key.sh",
+  "systemuser.sh", "user.sh", "keybox.sh", "aosp.sh", "patch.sh", "banned.sh",
+  "sus.sh", "boot_hash.sh",
+  "setprop.sh", "resetprop.sh", "pixelos.sh", "xiaomi.sh", "helluva.sh", "derpfest.sh",
+  "abnormal.sh", "app.sh", "prop.sh",
+  "support", "meowdump.sh", "meowverse.sh", "info.sh", "report.sh",
+  "selinux.sh", "module_info.sh", "resize.sh", "modal.sh",
+  "game.sh"
+];
