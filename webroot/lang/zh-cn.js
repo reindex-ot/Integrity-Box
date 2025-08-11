4dce85a336acb19db30ef23057166656f24ff6c4
MONA <121730102+MeowDump@users.noreply.github.com>
2025-08-11 19:17:18 +0530

    ‎

---
+++ webroot/lang/zh-cn.js
@@ -0,0 +1,69 @@
+// Language: Simplified Chinese
+// Translated by: AxelPLN
+
+// [SECTION 1] Translate the text inside " "
+export const translations = {
+  "zh-cn": [
+    "禁用自动白名单模式",
+    "启用自动白名单模式",
+    "设置自定义 PIF prop/json",
+    "欺骗/反欺骗 PIF Injection 值",
+    "欺骗/反欺骗 PIF Fork 值",
+    "干掉 GMS 进程",
+    "修复设备未认证",
+    "更新 Keybox",
+    "添加所有应用至 Target List",
+    "仅添加用户应用至 Target List",
+    "切换到默认 Keybox",
+    "切换到 AOSP Keybox",
+    "欺骗 Tricky Store 补丁",
+    "获取无效的 Keybox 列表",
+    "更新 SusFS 配置",
+    "设置已验证的 Boot 哈希",
+    "启用内置 GMS 欺骗",
+    "隐藏 GMS 属性检测",
+    "PixelOS 欺骗",
+    "Xiaomi EU 欺骗",
+    "HentaiOS 欺骗",
+    "Derpfest 欺骗",
+    "异常检测",
+    "标记的应用",
+    "属性检测",
+    "支持开发者",
+    "加入更新频道",
+    "一起闲聊",
+    "源代码",
+    "报告问题或 bug",
+    "更改 SELinux 状态",
+    "模块信息",
+    "调整 WebUI 显示大小",
+    "模态自动关闭切换",
+    "玩会游戏"
+  ]
+};
+
+// [SECTION 2] Translate the text inside { " " }
+export const buttonGroups = {
+  "Whitelist": { "zh-cn": "Shamiko & NoHello" },
+  "PIF": { "zh-cn": "Play Integrity Fix" },
+  "TS": { "zh-cn": "Tricky Store" },
+  "Sus": { "zh-cn": "SusFS" },
+  "CrSpoof": { "zh-cn": "自定义 ROM 欺骗" },
+  "Check": { "zh-cn": "诊断" },
+  "Help": { "zh-cn": "社区与帮助" },
+  "Basic": { "zh-cn": "通用" },
+  "TimePass": { "zh-cn": "打发时间" }
+};
+
+// [SECTION 3] There's nothing to translate in this section
+export const buttonOrder = [
+  "stop.sh", "start.sh", 
+  "pif.sh", "spoof.sh", "piffork.sh", "kill.sh", "vending.sh",  "key.sh",
+  "systemuser.sh", "user.sh", "keybox.sh", "aosp.sh", "patch.sh", "banned.sh",
+  "sus.sh", "boot_hash.sh",
+  "setprop.sh", "resetprop.sh", "pixelos.sh", "xiaomi.sh", "helluva.sh", "derpfest.sh",
+  "abnormal.sh", "app.sh", "prop.sh",
+  "support", "meowdump.sh", "meowverse.sh", "info.sh", "report.sh",
+  "selinux.sh", "module_info.sh", "resize.sh", "modal.sh",
+  "game.sh"
+];
