0e1b4e57881afb4181fef6778369d1cdbd3033fa
Re*Index. (ot_inc) <32851879+reindex-ot@users.noreply.github.com>
2025-09-08 23:05:36 +0900

    Add Japanese language

---
+++ webroot/lang/ja.js
@@ -0,0 +1,69 @@
+// Language: Japanese
+// Translated by: Re*Index.(ot_inc) @reindex-ot
+
+// [SECTION 1] Translate the text inside " "
+export const translations = {
+  "ja": [
+    "ホワイトリストモードを無効化",
+    "ホワイトリストモードを有効化",
+    "カスタム PIF フィンガープリントを設定",
+    "PIF インジェクションの値を偽装/偽装解除",
+    "PIF Fork の値を偽装/偽装解除",
+    "GMS プロセスを強制停止",
+    "デバイスが認定されていませんを修正",
+    "Keybox を更新",
+    "すべてのアプリをターゲットリストに追加",
+    "ユーザーアプリのみをターゲットリストに追加",
+    "デフォルトの Keybox に切り替え",
+    "オープンソース Keybox に切り替え",
+    "Tricky Store パッチを偽装",
+    "BAN 済みの Keybox リストを取得",
+    "SusFS 構成を更新",
+    "認証済みブートハッシュを設定",
+    "内蔵の GMS 偽装を有効化する",
+    "Hide Spoofing Props の検出",
+    "PixelOS の偽装",
+    "Xiaomi EU の偽装",
+    "HentaiOS の偽装",
+    "Derpfest の偽装",
+    "アブノーマルの検出",
+    "フラグ済みのアプリ",
+    "Props の検出",
+    "開発者をサポート",
+    "更新チャンネルに参加",
+    "ヘルプグループに参加",
+    "ソースコード",
+    "Issue/Bug で報告",
+    "SELinux ステータスを切り替え",
+    "モジュール情報",
+    "WebUI をリサイズ",
+    "モーダルを自動で閉じるを切り替え",
+    "Play Game"
+  ]
+};
+
+// [SECTION 2] Translate the text inside { }
+export const buttonGroups = {
+  "Whitelist": { "ja": "ホワイトリスト" },
+  "PIF": { "ja": "Play Integrity Fix" },
+  "TS": { "ja": "Tricky Store" },
+  "Sus": { "ja": "SusFS" },
+  "CrSpoof": { "ja": "カスタム ROM の偽装" },
+  "Check": { "ja": "Diagnostic" },
+  "Help": { "ja": "コミュニティとサポート" },
+  "Basic": { "ja": "基本" },
+  "TimePass": { "ja": "Game" }
+};
+
+// [SECTION 3] There's nothing to translate in this section
+export const buttonOrder = [
+  "stop.sh", "start.sh", 
+  "pif.sh", "spoof.sh", "piffork.sh", "kill.sh", "vending.sh",  "key.sh",
+  "systemuser.sh", "user.sh", "keybox.sh", "aosp.sh", "patch.sh", "banned.sh",
+  "sus.sh", "boot_hash.sh",
+  "setprop.sh", "resetprop.sh", "pixelos.sh", "xiaomi.sh", "helluva.sh", "derpfest.sh",
+  "abnormal.sh", "app.sh", "prop.sh",
+  "support", "meowdump.sh", "meowverse.sh", "info.sh", "report.sh",
+  "selinux.sh", "module_info.sh", "resize.sh", "modal.sh",
+  "game.sh"
+];
